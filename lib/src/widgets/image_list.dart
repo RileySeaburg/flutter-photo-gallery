4b9ae85f6d2f1c422f73d1beb9b14d2575fabd52
Riley Seaburg <=>
2022-08-09 19:03:31 -0400

    Update Photo border

--- lib/src/widgets/image_list.dart
+++ lib/src/widgets/image_list.dart
@@ -21,47 +21,64 @@ class ImageList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-      itemCount: photos.length,
-      itemBuilder: (context, int index) {
-        // check if the photos list is empty.
-        // if it is empty then display a message.
-        // if it is not empty then sort the list and display the first image.
-        if (photos.isEmpty) {
-          return Container(
-            child: Center(
-              child: Text('No Images Found'),
-            ),
-          );
-        } else {
-          var photoList = photos[index].photos;
-          if (photoList!.isEmpty) {
+        itemCount: photos.length,
+        itemBuilder: (context, int index) {
+          // check if the photos list is empty.
+          // if it is empty then display a message.
+          // if it is not empty then sort the list and display the first image.
+          if (photos.isEmpty) {
             return Container(
-              child: Center(
+              child: const Center(
                 child: Text('No Images Found'),
               ),
             );
           } else {
-            var firstPhoto = photoList[0];
-            if (firstPhoto == null) {
+            var photoList = photos[index].photos;
+            if (photoList!.isEmpty) {
               return Container(
-                child: Center(
+                child: const Center(
                   child: Text('No Images Found'),
                 ),
               );
             } else {
-            var firstPhotoUrl = firstPhoto.src?.landscape ?? 'fasdasd';
-              return Container(
-                child: Center(
-                  child: Image.network(
-                    firstPhotoUrl,
-                    fit: BoxFit.cover,
-                  ),
-                ),
-              );
+              var firstPhoto = photoList[0];
+              return buildImage(firstPhoto);
             }
           }
-        }
-      },
+        });
+  }
+
+  // build listview widget.
+  Widget buildImage(Photos photo) {
+    var firstPhotoUrl = photo.src?.landscape ?? 'fasdasd';
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: Colors.grey,
+          width: 1.0,
+        ),
+      ),
+      margin: const EdgeInsets.all(20.0),
+      padding: const EdgeInsets.all(20.0),
+      child: Center(
+          child: Column(
+        children:  <Widget>[
+          Padding (
+            padding: const EdgeInsets.only(bottom: 20.0),
+            child: Image.network(
+              firstPhotoUrl,
+              fit: BoxFit.cover,
+            ),
+          ),
+          Text(
+            photo.photographer.toString(),
+            style: const TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ],
+      )),
     );
   }
 }
