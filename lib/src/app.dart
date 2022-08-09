9670c35c2eb32d4a012ebaa420f735cc75366af3
Riley Seaburg <=>
2022-08-09 02:34:07 -0400

    Add photo gallery

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,12 +1,20 @@
 // Copyright (c) 2022 Evolving Software Corporation
-// 
+//
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
+// ignore_for_file: deprecated_member_use
+
 // Import flutter helper library.
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:http/http.dart' show get;
+import './models/image_model.dart';
+import './widgets/image_list.dart';
 
 class App extends StatefulWidget {
+  const App({Key? key}) : super(key: key);
+
   @override
   AppState createState() => AppState();
 }
@@ -14,33 +22,57 @@ class App extends StatefulWidget {
 // Create a class that will be the custom widget.
 // This class must extend the StatelessWidget class.
 class AppState extends State<App> {
-int counter = 0;
-
-
-
-
+  int counter = 0;
+  List<PexelsModel> photos = [];
+  void fetchImage() async {
+    counter++;
+    Uri uri = Uri.https(
+      'api.pexels.com',
+      '/v1/search',
+      {
+        'curated': 'true',
+        'query': 'nature',
+        'per_page': '50',
+        'page': '$counter',
+      },
+    );
+    var httpResponse = await get(
+        uri,
+        headers: {
+          'Authorization':
+              '563492ad6f917000010000014a2dac3ddf21465cabc3c5081122f753',
+        });
+    // instantiate the model class. if the ersponse contains html then throw an exception and print the error.
+    if (httpResponse.statusCode == 200) {
+      var decodedJson = json.decode(httpResponse.body);
+      var pexelsModel = PexelsModel.fromJson(decodedJson);
+      setState(() {
+        photos.add(pexelsModel);
+      });
+    } else {
+      print('Error Original Requested URL: ${uri.toString()}');
+      // print('Error: ${httpResponse.statusCode}');
+      // print(httpResponse.statusCode);
+      // print(httpResponse.body);
+      throw Exception('Failed to load photos');
+    }
+  }
 // Must define a build method that returns a widget.
 // This widget is what is shown on the screen.
 
-@override
-Widget build(BuildContext context) {
-  return MaterialApp(
-    home: Scaffold(
-      body: Text('$counter'),
-      appBar: AppBar(
-        title: Text('My First App'),
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: ImageList(photos),
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: fetchImage,
+        ),
       ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {
-          setState(() {
-            // This call to setState tells Flutter that something has changed in this State,
-            // and that it should rerun the build method to update the UI with the new values.
-            counter += 1;
-          });
-        },
-      ),
-    ),
-  );
-}
+    );
+  }
 }
---
+++ lib/src/widgets/image_list.dart
@@ -0,0 +1,67 @@
+// Copyright (c) 2022 Evolving Software Corporation
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+// ignore_for_file: avoid_unnecessary_containers, prefer_const_constructors_in_immutables, use_key_in_widget_constructors
+
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import '../models/image_model.dart';
+
+/// ## ImageList
+/// ### This widget is a custom widget that displays a list of images.
+class ImageList extends StatelessWidget {
+  // Make all the fields immutable by adding 'final'.
+  final List<PexelsModel> photos;
+  ImageList(this.photos);
+
+  // build listview widget.
+  @override
+  Widget build(BuildContext context) {6
+    return ListView.builder(
+      itemCount: photos.length,
+      itemBuilder: (context, int index) {
+        // check if the photos list is empty.
+        // if it is empty then display a message.
+        // if it is not empty then sort the list and display the first image.
+        if (photos.isEmpty) {
+          return Container(
+            child: Center(
+              child: Text('No Images Found'),
+            ),
+          );
+        } else {
+          var photoList = photos[index].photos;
+          if (photoList!.isEmpty) {
+            return Container(
+              child: Center(
+                child: Text('No Images Found'),
+              ),
+            );
+          } else {
+            var firstPhoto = photoList[0];
+            if (firstPhoto == null) {
+              return Container(
+                child: Center(
+                  child: Text('No Images Found'),
+                ),
+              );
+            } else {
+            var firstPhotoUrl = firstPhoto.src?.landscape ?? 'fasdasd';
+              return Container(
+                child: Center(
+                  child: Image.network(
+                    firstPhotoUrl,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              );
+            }
+          }
+        }
+      },
+    );
+  }
+}
